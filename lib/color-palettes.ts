a7704a4c90692aee38837e10fdc844b284c69209
Florian Martin <fmartin@semeia.io>
2021-06-26 13:52:34 +0200

    🎨 Add color palettes.

---
+++ lib/color-palettes.ts
@@ -0,0 +1,10 @@
+import p5 from "p5";
+
+const colors: Array<Array<string>> = [
+  ["#EC048C", "#54175D"],
+  ["#161F33", "#FE5CBA"],
+  ["#DC0193", "#5AF0FB"],
+  ["#EB0057", "#FEE2A1"],
+];
+
+export default colors;
